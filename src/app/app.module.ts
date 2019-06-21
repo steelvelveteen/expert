ab50f61fdb36fa6dc7443c2357635c6efeaa8619
Jose <jvico@da.com.au>
2019-06-21 16:50:19 +0930

    Clean up app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 import { MaterialModule } from './material/material.module';
+
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { MenuComponent } from './menu/menu.component';
-import { HttpClientModule } from '@angular/common/http';
 import { FormsComponent } from './content/angular/forms/forms.component';
 import { RoutingComponent } from './content/angular/routing/routing.component';
 import { HttpComponent } from './content/angular/http/http.component';
