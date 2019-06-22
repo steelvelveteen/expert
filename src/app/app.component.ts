1e7cbde97a88e64ad398836beee1ecf6170e2f5f
steelvelveteen <joeyvico@gmail.com>
2019-06-22 18:36:55 +0930

    Undo last changes and commented them out for now

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { DataService } from './shared/services/data.service';
-import { IArea } from './shared/domain/models/area.model';
-import { HttpErrorResponse } from '@angular/common/http';
+// import { Router } from '@angular/router';
+// import { DataService } from './shared/services/data.service';
+// import { IArea } from './shared/domain/models/area.model';
+// import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -11,12 +11,24 @@ import { HttpErrorResponse } from '@angular/common/http';
 })
 export class AppComponent implements OnInit{
  
-  private areas = [];
-  private httpError: HttpErrorResponse = null;
+  // private areas = [];
+  // private httpError: HttpErrorResponse = null;
 
-  constructor(private _dataService: DataService, private _router: Router) {}
+  // constructor(private _dataService: DataService, private _router: Router) {}
 
   ngOnInit() {
+    // this._dataService.fetchData().
+    // subscribe(
+    //   (data: IArea[]) => {
+    //     this.areas = data;
+    //   this._router.navigateByUrl(this.areas[0].section[0].url);
+
+    //   },
+    //   (error: HttpErrorResponse) => {
+    //     this.httpError = error;
+    //     console.log(error);
+    //   }
+    // );
   }
  
 }
