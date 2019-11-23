dc18216bc14df44d67b2a8537508bd3ced50ec8d
steelvelveteen <joeyvico@gmail.com>
2019-11-23 18:25:36 +1030

    Added mat-select module to the custom material module of mine

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
+import { MatSelectModule } from '@angular/material/select';
 import { 
   MatButtonModule,
   MatFormFieldModule,
   MatInputModule,
-  MatProgressSpinnerModule
+  MatProgressSpinnerModule,
+  
 } from '@angular/material';
 
 const material = [ 
   MatButtonModule, 
   MatFormFieldModule, 
   MatInputModule,
-  MatProgressSpinnerModule
+  MatProgressSpinnerModule,
+  MatSelectModule
 ];
 
 @NgModule({
